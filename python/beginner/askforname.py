c49009c5c9d0bb2075273b93f465db0b85d65294
Paul Kramme <pjkramme@gmail.com>
2016-09-07 16:02:42 +0200

    Add empty check

--- python/beginner/askforname.py
+++ python/beginner/askforname.py
@@ -2,4 +2,7 @@
 
 print("Hey there! What's your name?")
 name = input()
-print("Hello " + name + "! Nice to meet you!")
+if name == "":
+	print("You can not have an empty name...")
+else:
+	print("Hello " + name + "! Nice to meet you!")
